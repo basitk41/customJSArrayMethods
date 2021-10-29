77fea17429324bf729f3fc3c02f481f0fcfb7fdc
Basit Ali <basitk41@gmail.com>
2021-10-29 23:29:50 +0500

    custom concat method

---
+++ concat.js
@@ -0,0 +1,10 @@
+// custom method implementation.
+Array.prototype.myConcat = function (arr) {
+  return [...this, ...arr];
+};
+
+// example
+const array = [1, 2, 3];
+const result = array.myConcat([4, 5]);
+console.log(result);
+console.log(array);
