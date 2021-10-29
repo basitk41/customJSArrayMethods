90808a055b875461ed8c697f89982f47b70f1cef
Basit Ali <basitk41@gmail.com>
2021-10-29 22:12:47 +0500

    bug fix

--- map.js
+++ map.js
@@ -1,12 +1,14 @@
 // custom method implementation.
 Array.prototype.myMap = function (callback) {
+  let result = [];
   for (let i = 0; i < this.length; i++) {
-    this[i] = callback(this[i], i, this);
+    result[i] = callback(this[i], i, this);
   }
-  return this;
+  return result;
 };
 
 // example
 const array = [1, 2, 3];
 const result = array.myMap((x) => x * 2);
 console.log(result);
+console.log(array);
