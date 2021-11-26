eb71d68b2b22719467494f224baa048bb1a35790
Basit Ali <basitk41@gmail.com>
2021-11-26 21:34:24 +0500

    refactoring.

--- push.js
+++ push.js
@@ -1,12 +1,10 @@
 // custom method implementation.
 Array.prototype.myPush = function (...x) {
-  for (let i = 0; i < x.length; i++) {
-    this[this.length] = x[i];
-  }
+  for (let i = 0; i < x.length; i++) this[this.length] = x[i];
   return this.length;
 };
 
 // example
 const array = [1, 2, 3];
-console.log(array.myPush(4, 5));
+console.log(array.myPush(4, 5, 6));
 console.log(array);
