09a799e89908407bc88a7596388c27072553257b
Basit Ali <basitk41@gmail.com>
2021-11-27 00:55:24 +0500

    refactoring.

--- splice.js
+++ splice.js
@@ -1,31 +1,33 @@
 // custom method implementation.
 Array.prototype.mySplice = function (...args) {
   let [index, del, ...elements] = args,
-    result = [],
-    arr = [],
-    test = [],
-    n = 0,
-    m = 0;
+    removed = [],
+    remaining = [],
+    converted = [],
+    m = 0,
+    n = 0;
   if (del >= this.length) del = this.length - 1;
   if (this.length === 0) (del = 0), (index = 0);
   if (del < 0) del = 0;
   let length = this.length + elements.length - del;
-  for (let a = 0; a < this.length; a++) {
-    if (a >= index && a <= del && del !== 0 && del !== result.length)
-      result[result.length] = this[a];
-    else test[test.length] = this[a];
+  for (let i = 0; i < this.length; i++) {
+    if (i >= index && i <= del && del !== 0 && del !== removed.length)
+      removed[removed.length] = this[i];
+    else remaining[remaining.length] = this[i];
   }
-  for (let i = 0; i < length; i++) {
-    if (i >= index && n < elements.length) (arr[i] = elements[n]), n++;
-    else (arr[i] = test[m]), m++;
-    this[i] = arr[i];
+  for (let j = 0; j < length; j++) {
+    if (j >= index && n < elements.length) (converted[j] = elements[n]), n++;
+    else (converted[j] = remaining[m]), m++;
+    this[j] = converted[j];
   }
   this.length = length;
-  return result;
+  return removed;
 };
 
 // example
 const arr = [1, 4];
-console.log("return", arr.mySplice(0, 0, 2));
-// console.log(arr.splice(1, 3, 4, 5));
+console.time("Time Complexity: ");
+console.log("return", arr.mySplice(1, 0, 2));
+// console.log(arr.splice(1, 0, 2));
+console.timeEnd("Time Complexity: ");
 console.log("original arr", arr);
