443005964a4a83e636bc78bd96039c3a2c653c2d
Basit Ali <basitk41@gmail.com>
2021-11-26 21:34:20 +0500

    refactoring.

--- pop.js
+++ pop.js
@@ -1,11 +1,12 @@
 // custom method implementation.
 Array.prototype.myPop = function () {
   let x = this[this.length - 1];
-  this.length = this.length - 1;
+  this.length = this.length > 0 ? this.length - 1 : 0;
   return x;
 };
 
 // example
-const array = [1, 2, 3];
+const array = [1, 2];
 console.log(array.myPop());
+// console.log(array.pop());
 console.log(array);
