c4474cf0a96f731b301bc657a9bf197aa44094a9
Basit Ali <basitk41@gmail.com>
2021-11-26 21:34:28 +0500

    refactoring.

--- shift.js
+++ shift.js
@@ -1,14 +1,13 @@
 // custom method implementation.
 Array.prototype.myShift = function () {
   const x = this[0];
-  for (let i = 1; i < this.length; i++) {
-    this[i - 1] = this[i];
-  }
-  this.length = this.length - 1;
+  for (let i = 1; i < this.length; i++) this[i - 1] = this[i];
+  this.length = this.length > 0 ? this.length - 1 : 0;
   return x;
 };
 
 // example
-const array = [1, 2, 3];
+const array = [1, 2, 3, 4];
 console.log(array.myShift());
+// console.log(array.shift());
 console.log(array);
