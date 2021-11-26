cd4b35f43fdcad541eb9d31cfcf1f3f41a8ea806
Basit Ali <basitk41@gmail.com>
2021-11-26 21:34:37 +0500

    refactoring.

--- unshift.js
+++ unshift.js
@@ -1,14 +1,12 @@
 // custom method implementation.
 Array.prototype.myUnshift = function (...args) {
   const arr = [...args, ...this];
-  for (let i = 0; i < arr.length; i++) {
-    this[i] = arr[i];
-  }
+  for (let i = 0; i < arr.length; i++) this[i] = arr[i];
   return this.length;
 };
 
 // example
-const array = [1, 2, 3];
+const array = [3, 4, 5];
 console.log(array.myUnshift(1, 2));
 // console.log(array.unshift(1, 2));
 console.log(array);
